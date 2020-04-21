697865ddebbc8ae0af64f1e6c1d95ad38e73e7b1
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-21 10:08:12 +0300

    Add the test case to test if the object has been saved to the user list

--- test.py
+++ test.py
@@ -16,3 +16,13 @@ class TestUser(unittest.TestCase):
         '''
         test_init test case to test if the object is initialized properly
         '''    
+        self.assertEqual(self.new_user.user_name,"kevzzz")
+        self.assertEqual(self.new_user.password,"ximss")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''    
+if __name__ == '__main__':
+    unittest.main()

5f11febfcb528d5409ded660232c4163e0310e8c
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-21 09:49:24 +0300

    Add the user info

--- user.py
+++ user.py
@@ -4,3 +4,7 @@ class User:
     Class that generates new instances of user.
     """
     user_list = [] # Empty contact list
+
+  def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password

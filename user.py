2d020b6a0704e7ec759ca80dfa3f2864d640ed46
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-21 09:51:00 +0300

    Add the save user info details

--- user.py
+++ user.py
@@ -5,6 +5,12 @@ class User:
     """
     user_list = [] # Empty contact list
 
-  def __init__(self,user_name,password):
+    def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+
+    def save_user (self):
+        '''
+        save user method saves contact
+        '''
+        User.user_list.append(self)
